235f50c95536b79bad8ed16f116bc4118d54172b
Karim Tayie <karimsherif2597@gmail.com>
2019-08-01 02:01:08 +0200

    Added user login api

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,4 +7,5 @@ app_name = 'user'
 
 urlpatterns = [
     path('register/', views.CreateUserView.as_view(), name='register'),
+    path('login/', views.CreateTokenView.as_view(), name='login'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,9 @@
 from django.core.mail import send_mail
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, AuthTokenSeriallizer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -19,5 +21,10 @@ class CreateUserView(generics.CreateAPIView):
             'test@email.com',
             [email]
         )
-        
         serializer.save()
+        
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSeriallizer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
