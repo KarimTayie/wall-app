4e7db1da41d2c679591390dff24eb68091dd897b
Karim Tayie <karimsherif2597@gmail.com>
2019-08-02 14:30:11 +0200

    Updated wallserializer

--- app/wall/serializers.py
+++ app/wall/serializers.py
@@ -5,8 +5,14 @@ from core.models import Wall
 
 class WallSerializer(serializers.ModelSerializer):
     """Serializer for wall objects"""
+    # user = serializers.CharField(max_length=255)
+    username = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='name',
+        source='user'
+    )
 
     class Meta:
         model = Wall
-        fields = ('id', 'title', 'body')
+        fields = ('id', 'username', 'title', 'body')
         read_only_fields = ('id',)
